ff8d85ab4e3bab1b38a59e4cde7a48313c84455e
Stephan Eberwijn <11700946@student.pxl.be>
2021-03-15 16:31:27 +0100

    Documented code + fixed detailedbox height + made mockup data

--- js.js
+++ js.js
@@ -1,15 +1,125 @@
 //Variables
 const cursor = document.getElementById("cursor");
-
+const previousButtons = document.querySelectorAll('button.prev');
+const nextButtons = document.querySelectorAll('button.next');
+const items = document.querySelectorAll('div.item');
 const detailedBox = document.getElementById('detailedBox');
 const smallLine = document.getElementById('smallLine');
 
-const previousButtons = document.querySelectorAll('button.prev');
-const nextButtons = document.querySelectorAll('button.next');
+let cursorInactiveTimer;
 
-const items = document.querySelectorAll('div.item');
+
+//Initialize page
+cursor.style.display = 'none';
 let isMoving = false;
 
+let Carousel = {
+    width: 200, // Images are forced into a width of this many pixels.
+    numVisible: 3, // The number of images visible at once.
+    duration: 750, // Animation duration in milliseconds.
+    itemMargin: 40
+};
+
+let itemNames = ['Queens Ring', 'Kings Ring', 'Black Widow Ring', 'Hercules Ring', 'Brothers Alu Ring'];
+let itemCodeNumber = 0;
+
+//Runs when the window is loaded.
+window.onload = function() {
+    //Make the carousel as it should be, with items in it.
+    var carousel = Carousel.carousel = document.getElementById('carousel'),
+        items = carousel.querySelectorAll('div.item'),
+        numItems = items.length,
+        //itemHeight = 150,
+        //padding = Carousel.padding,
+        //rowHeight = Carousel.rowHeight = itemHeight + 2 * padding;
+        //rowHeight = Carousel.rowHeight = 2 * itemHeight + 2 * padding;
+        itemWidth = Carousel.width;
+    carousel.style.width = itemWidth + 'px';
+
+    //Goes over every item in the items array.
+    for (var i = 0; i < numItems; ++i) {
+        let itemData = createNewItemMockData();
+        var item = items[i],
+            imageHolder = document.createElement('div');
+
+        //Give the item an 'id' and 'margin'.
+        item.id = itemData.itemCode;
+        item.style.marginTop = Carousel.itemMargin / 2 + "px";
+        item.style.marginBottom = Carousel.itemMargin / 2 + "px";
+
+        //Add the image holder div
+        imageHolder.className = 'imgHolder';
+        item.appendChild(imageHolder);
+
+        //Add item image
+        var img = document.createElement('img');
+        for (let index = 0; index < 3; index++) {
+            img.src = `./Images/ring${i % 3}.png`;
+        }
+
+        imageHolder.appendChild(img);
+
+        //Add the item name "p"-tag
+        let itemNameTag = document.createElement('p');
+        console.log(itemData.itemName.length)
+        if (itemData.itemName.length > 12) {
+            itemNameTag.innerHTML = itemData.itemName.substring(0, 10) + "...";
+        } else {
+            itemNameTag.innerHTML = itemData.itemName;
+        }
+        item.appendChild(itemNameTag);
+    }
+
+    carousel.style.alignItems = 'center';
+
+
+    //TODO: Might throw NullPointerException
+    Carousel.rowHeight = getDistanceBetweenElements(
+        document.querySelectorAll("div.item")[0],
+        document.querySelectorAll("div.item")[1]
+    );
+    carousel.style.height = Carousel.numVisible * Carousel.rowHeight + 'px';
+    carousel.style.visibility = 'visible';
+    carousel.style.overflow = 'hidden';
+
+    let wrapper = Carousel.wrapper = document.createElement('div');
+    wrapper.id = 'carouselWrapper';
+    wrapper.style.width = carousel.style.width;
+    wrapper.style.height = carousel.offsetHeight + 'px';
+    wrapper.style.marginBottom = Carousel.itemMargin / 2 + "px";
+    carousel.parentNode.insertBefore(wrapper, carousel);
+    wrapper.appendChild(carousel);
+
+    let firstItem = document.querySelectorAll("div.item")[0].getBoundingClientRect();
+
+    detailedBox.style.top = firstItem.top + 'px';
+
+    //60 is the width of the 'smallLine'.
+    detailedBox.style.left = (firstItem.right + 60) + 'px';
+    //detailedBox.style.marginTop = Carousel.itemMargin / (Carousel.numVisible * 3) + 'px';
+    detailedBox.style.height = (Carousel.numVisible * Carousel.rowHeight) - 24.5 + 'px';
+    detailedBox.style.opacity = 0;
+    detailedBox.style.transition = '1000ms';
+};
+
+function createNewItemMockData() {
+    let ItemMockData = {
+        itemName: itemNames[Math.floor(Math.random() * Math.floor(itemNames.length))],
+        itemCode: createItemCodeString(),
+        itemTags: ['Ring'],
+    }
+    itemCodeNumber++;
+    return ItemMockData;
+}
+
+function createItemCodeString() {
+    let codeString = itemCodeNumber.toString();
+    for (let index = 0; codeString.length < 12; index++) {
+        codeString = `0${codeString}`;
+    }
+    return codeString;
+}
+//Adds an event listener to every previous-button for when a transition ends.
 previousButtons.forEach(element => {
     element.addEventListener("transitionend", function() {
         if (!isMoving) {
@@ -23,6 +133,7 @@ previousButtons.forEach(element => {
     });
 });
 
+//Adds an event listener to every next-button for when a transition ends.
 nextButtons.forEach(element => {
     element.addEventListener("transitionend", function() {
         if (!isMoving) {
@@ -36,19 +147,21 @@ nextButtons.forEach(element => {
     })
 });
 
-
-
+//Adds an event listener to every item-box for when an animation ends.
 items.forEach(element => {
     element.addEventListener("animationend", function() {
         var selectedItemCoords = getCoords(element);
-        let smallLine = moveSmallLine(selectedItemCoords).getBoundingClientRect();
 
-        moveDetailedBox(document.querySelectorAll('div.item')[0].getBoundingClientRect(), smallLine.width);
+        //TODO: hide small line, move small line, show small line
+        hideSmallLine();
+        moveSmallLine(selectedItemCoords);
+        showSmallLine();
+        showDetailedBox();
 
     }, false);
 });
 
-
+//Gets the coordinates of an element.
 function getCoords(elem) {
     let box = elem.getBoundingClientRect();
     return {
@@ -59,53 +172,56 @@ function getCoords(elem) {
     };
 }
 
+//Moves the "small-line" to the middle right of the selected item-box.
 function moveSmallLine(itemCoords) {
     smallLine.style.top = (itemCoords.top + itemCoords.bottom) / 2 + 'px';
     smallLine.style.left = itemCoords.right + 'px';
     smallLine.style.display = 'block';
     smallLine.style.transition = '1000ms';
-    smallLine.style.opacity = 1;
-    return smallLine;
 }
 
-function moveDetailedBox(firstItemCoords, smallLineWidth) {
-    //TODO: Currently only for left side.
-    detailedBox.style.top = firstItemCoords.top + 'px';
-    detailedBox.style.left = (firstItemCoords.right + smallLineWidth) + 'px';
-    detailedBox.style.height = (3 * firstItemCoords.height + 2 * 50 - 5) + 'px';
-    detailedBox.style.display = 'block';
+//Shows small-line
+function showSmallLine() {
+    smallLine.style.opacity = 1;
+}
 
+//Moves the "detailed-box". TODO:make this an initialization thing.
+function showDetailedBox() {
+    //TODO: Currently only for left side.
+    detailedBox.style.display = 'block';
     detailedBox.style.opacity = 1;
 }
 
+//Whenever the user clicks on the body of the page, it hides the detailed box and small-line.
+//TODO: make this a hover thing
 window.document.body.addEventListener('click', function() {
     hideDetailedBox();
+    hideSmallLine();
 });
 
+//Hides the "detailed-box".
 function hideDetailedBox() {
     //detailedBox.style.display = 'none';
-    //smallLine.style.display = 'none';
-
     detailedBox.style.opacity = 0;
+}
+
+//Hides the "small-line".
+function hideSmallLine() {
+    //smallLine.style.display = 'none';
     smallLine.style.opacity = 0;
 }
 
 // ++=-CURSOR-=++
 
-
-//Initialize page
-cursor.style.display = 'none';
-
-
 //Turn on/off the cursor when active/inactive
-let timeout;
 document.onmousemove = function() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function() { cursor.style.display = 'none'; }, 5000);
+    clearTimeout(cursorInactiveTimer);
+    cursorInactiveTimer = setTimeout(function() { cursor.style.display = 'none'; }, 5000);
 }
 
 //Move the cursor
 document.addEventListener('mousemove', e => {
+    //Set the cursor div on the position of the mouse.
     cursor.setAttribute("style", `top: ${e.pageY - 15}px; left: ${e.pageX - 15}px`);
     //Check for each item-box if the cursor is colliding.
     document.querySelectorAll('div.item').forEach(item => {
@@ -142,9 +258,9 @@ document.addEventListener('mousemove', e => {
             }
         }
     });
-
 });
 
+//Checks if element "a" is colliding with element "b".
 function isColliding(a, b) {
     return !(
         ((a.y + a.height) < (b.y)) ||
@@ -154,88 +270,10 @@ function isColliding(a, b) {
     );
 }
 
-
-// ++=-CAROUSEL-=++
-
-var Carousel = {
-    width: 200, // Images are forced into a width of this many pixels.
-    numVisible: 3, // The number of images visible at once.
-    duration: 750, // Animation duration in milliseconds.
-    padding: 200, // Vertical padding around each image, in pixels.
-    itemMargin: 99
-};
-
-window.onload = function() {
-    var carousel = Carousel.carousel = document.getElementById('carousel'),
-        items = carousel.querySelectorAll('div.item'),
-        numItems = items.length,
-        itemWidth = 150,
-        itemHeight = 150,
-        padding = Carousel.padding,
-        //rowHeight = Carousel.rowHeight = itemHeight + 2 * padding;
-        rowHeight = Carousel.rowHeight = 2 * itemHeight + 2 * padding;
-    carousel.style.width = itemWidth + 'px';
-
-    for (var i = 0; i < numItems; ++i) {
-        var item = items[i],
-            imageHolder = document.createElement('div');
-
-        item.id = i;
-        item.style.marginTop = Carousel.itemMargin / 2 + "px";
-        item.style.marginBottom = Carousel.itemMargin / 2 + "px";
-
-        //Add the image holder div
-        imageHolder.className = 'imgHolder';
-        item.appendChild(imageHolder);
-
-        //Add item image
-        var img = document.createElement('img');
-        for (let index = 0; index < 3; index++) {
-            img.src = `./Images/ring${i % 3}.png`;
-        }
-
-        imageHolder.appendChild(img);
-
-        //Add the item name "p"-tag
-        let itemName = document.createElement('p');
-        itemName.innerHTML = "Silver Ring " + i;
-        item.appendChild(itemName);
-    }
-
-
-
-    carousel.style.width = 200 + 'px';
-    carousel.style.alignItems = 'center';
-
-
-    //TODO: Might throw NullPointerException
-    Carousel.rowHeight = getDistanceBetweenElements(
-        document.querySelectorAll("div.item")[0],
-        document.querySelectorAll("div.item")[1]
-    );
-    carousel.style.height = Carousel.numVisible * Carousel.rowHeight + 'px';
-    carousel.style.visibility = 'visible';
-    carousel.style.overflow = 'hidden';
-
-
-
-
-    var wrapper = Carousel.wrapper = document.createElement('div');
-    wrapper.id = 'carouselWrapper';
-    wrapper.style.width = carousel.style.width;
-    wrapper.style.height = carousel.offsetHeight + 'px';
-    wrapper.style.marginBottom = Carousel.itemMargin / 2 + "px";
-    carousel.parentNode.insertBefore(wrapper, carousel);
-    wrapper.appendChild(carousel);
-
-
-
-};
-
 function animate(begin, end, finalTask) {
+    hideSmallLine();
     hideDetailedBox();
-    var wrapper = Carousel.wrapper,
-        carousel = Carousel.carousel,
+    let carousel = Carousel.carousel,
         change = end - begin,
         duration = Carousel.duration,
         startTime = Date.now();
