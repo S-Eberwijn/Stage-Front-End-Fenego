ea475bb4efed5152e471cbe498bb5bbc0d2fb5e6
Anthony Lambrechts <anthonylambrechts01@gmail.com>
2021-03-25 15:42:04 +0100

    Scanner with video feed

--- Scanner/scanner.js
+++ Scanner/scanner.js
@@ -1,23 +1,34 @@
-Quagga.init({
-    inputStream : {
-        name : "Live",
-        type : "LiveStream",
-        target: document.querySelector('#video')    // Or '#yourElement' (optional)
-    },
-    decoder : {
-        readers : ["code_128_reader"]
-    }
-}, function(err) {
+Quagga.init(this.state,function(err) {
     if (err) {
         console.log(err);
-        return
     }
-    console.log("Initialization finished. Ready to start");
     Quagga.start();
 });
 
+
+
 Quagga.onProcessed(function(result) {
-    console.log(result);
+    var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+    if (result) {
+        if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+            result.boxes.filter(function (box) {
+                return box !== result.box;
+            }).forEach(function (box) {
+                Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+            });
+        }
+
+        if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+        }
+    }
 });
 
 
