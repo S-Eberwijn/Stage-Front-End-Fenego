fb18a953b460e83f2c14ad6312a94ed584298079
AnthonyLambrechts-pxl <43882355+AnthonyLambrechts-pxl@users.noreply.github.com>
2021-04-27 10:45:55 +0200

    Initial face recognition

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var favicon = require('serve-favicon');
+let fs = require('fs');
 
 var app = express();
 
@@ -17,6 +18,32 @@ app.get('/barcode', function (req, res) {
 
 app.get('/startscreen', function (req, res) {
     res.sendFile('startscreen.html', { root: `${__dirname}\\public` });
+});
+
+app.get('/face', function (req, res) {
+    res.sendFile('face.html', { root: `${__dirname}\\public` });
+});
+
+app.get('/face2', function (req, res) {
+    console.log("face2");
+    try {
+        console.log("Try")
+        fs.unlinkSync("./something.jpg");
+    } catch (e){
+        console.log('catch');
+        res.sendFile('face2.html', { root: `${__dirname}\\public` });
+    }
+    finally {
+        console.log("finally");
+        let oldPath = 'C:/Users/11802010/Downloads/screenshot.jpg'
+        let newPath = './something.jpg'
+
+        fs.rename(oldPath, newPath, function (err) {
+            // if (err) throw err
+            console.log('Successfully renamed - AKA moved!')
+        })
+        res.sendFile('face2.html', { root: `${__dirname}\\public` });
+    }
 });
 
 app.listen(3000, function () {
---
+++ public/js/face/face2.js
@@ -0,0 +1,55 @@
+const imageUpload = document.getElementById('imageUpload')
+
+Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('models')
+]).then(start)
+
+async function start() {
+    console.log("ggg");
+    const container = document.createElement('div')
+    container.style.position = 'relative'
+    document.body.append(container)
+    const labeledFaceDescriptors = await loadLabeledImages()
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+    let image
+    let canvas
+    document.body.append('Loaded')
+    imageUpload.addEventListener('click', async () => {
+
+        //TODO: kijk naar original code en fiks deze nest
+        if (image) image.remove()
+        if (canvas) canvas.remove()
+        // image = await faceapi.bufferToImage("/something.jpg")
+        image = document.createElement("image");
+        image.src = "/something.jpg"
+        container.append(image)
+        canvas = faceapi.createCanvasFromMedia(image)
+        container.append(canvas)
+        const displaySize = { width: image.width, height: image.height }
+        faceapi.matchDimensions(canvas, displaySize)
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+        results.forEach((result, i) => {
+            const box = resizedDetections[i].detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+            drawBox.draw(canvas)
+        })
+    })
+}
+
+function loadLabeledImages() {
+    const labels = ['Anthony']
+    return Promise.all(
+        labels.map(async label => {
+            const descriptions = []
+            const img = await faceapi.fetchImage(`./labeled_images/Anthony/Reference.jpg`)
+            const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+            descriptions.push(detections.descriptor)
+
+            return new faceapi.LabeledFaceDescriptors(label, descriptions)
+        })
+    )
+}
