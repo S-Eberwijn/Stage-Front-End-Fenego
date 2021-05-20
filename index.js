76c4fcd36f45d5885a9b54cc492ec4f50f57b843
Anthony Lambrechts <anthonylambrechts01@gmail.com>
2021-05-20 10:53:28 +0200

    Python script added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var favicon = require('serve-favicon');
-// var open = require('open');
 var childProcess = require('child_process');
+const { spawn } = require('child_process')
+const logOutput = (name) => (data) => console.log(`[${name}] ${data.toString()}`);
+let pythonProcess;
+function run() {
+    pythonProcess = spawn('python', ['python/media/main.py',"mediapipe","cv2","pypiwin32"]);
 
+    pythonProcess.stdout.on(
+        'data',
+        logOutput('stdout')
+    );
+
+    pythonProcess.stderr.on(
+        'data',
+        logOutput('stderr')
+    );
+};
 
 var app = express();
 
@@ -15,6 +29,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/barcode', function (req, res) {
+    pythonProcess.kill('SIGTERM');
     res.sendFile('barcode.html', { root: `${__dirname}\\public` });
 });
 
@@ -30,6 +45,9 @@ app.get('/findego', function (req, res) {
 app.get('/face', function (req, res) {
     res.sendFile('face.html', { root: `${__dirname}\\public` });
 });
+app.get('/run', function (req, res) {
+    run();
+});
 
 // Default every route except the above to serve the 404.html
 app.get('*', function (req, res) {
@@ -40,5 +58,5 @@ app.listen(3000, function () {
     console.log("Listening on port 3000!")
 });
 
-// open('http://localhost:3000'); //open normal tab
-childProcess.exec('start chrome --kiosk http;//localhost:3000'); //open in kiosk mode, ctrl+W to close or close in taskbar 2nd screen
+
+childProcess.exec('start chrome --kiosk http;//localhost:3000/run'); //open in kiosk mode, ctrl+W to close or close in taskbar 2nd screen
---
+++ python/media/main.py
@@ -0,0 +1,52 @@
+import os
+import sys
+os.system('python3 -m pip install {}'.format(sys.argv[1]))
+os.system('python3 -m pip install {}'.format(sys.argv[2]))
+os.system('python3 -m pip install {}'.format(sys.argv[3]))
+import mediapipe as mp
+import cv2
+import win32api, win32con
+
+
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+
+        # Flip the image horizontally for a later selfie-view display, and convert
+        # the BGR image to RGB.
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        results = hands.process(image)
+
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            # if results.multi_hand_landmarks[0]:
+            # print(results.multi_hand_landmarks[0][5])
+
+            for hand_landmarks in results.multi_hand_landmarks:
+                indexX = int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * 2000)
+                indexY = int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * 1000)
+
+                win32api.SetCursorPos((indexX, indexY))
+                mp_drawing.draw_landmarks(
+                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        cv2.imshow('MediaPipe Hands', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+cap.release()
